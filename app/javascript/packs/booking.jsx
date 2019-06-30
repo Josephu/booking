75975c054cdf2668f2bc6157b9a1e81aef66d93e
Jimmy Wong <imagegospel@gmail.com>
2019-06-30 11:40:34 +1000

    Make toggle work

--- app/javascript/packs/booking.jsx
+++ app/javascript/packs/booking.jsx
@@ -5,7 +5,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
-import { Container, Row, Col, Button, Card, CardBody, CardTitle, CardImg } from 'reactstrap';
+import { Container, Row, Col, Button, Card, CardBody, CardTitle, CardImg, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
@@ -28,7 +28,6 @@ class App extends React.Component {
      return (
       <Container className="App">
         <HotelList data={this.state.hotels}/>
-        <Button color="danger">Danger!!</Button>
       </Container>
      );
    }
@@ -36,7 +35,6 @@ class App extends React.Component {
 
 class HotelList extends React.Component {
   render() {
-    console.log(this.props.data)
     const hotels = this.props.data.map((hotel) =>
       <Hotel data={hotel} key={hotel.uuid}/>
     );
@@ -49,23 +47,28 @@ class HotelList extends React.Component {
 }
 
 class Hotel extends React.Component {
+  state = {
+    isModalOpen: false
+  };
+  // Enable parent to trigger child function
+  setupModalToggle = (toggleModal) => {
+    this.toggleModal = toggleModal
+  }
   render() {
     return (      
       <Card body className="mb-3">
         <Row className="no-gutters">
-        {/* <div className="hotel">
-          <h1>{this.props.data.name}</h1>
-          <img src={this.props.data.cover_image} />
-        </div> */}
-        <Col sm="3">
-          <CardImg top src={this.props.data.cover_image} />
-        </Col>
-        <Col sm="6">
-          <CardBody>
-            <CardTitle>{this.props.data.name}</CardTitle>
-          </CardBody>
-        </Col>        
+          <Col sm="3">
+            <CardImg top src={this.props.data.cover_image} />
+          </Col>
+          <Col sm="6">
+            <CardBody>
+              <CardTitle>{this.props.data.name}</CardTitle>
+              <Button color="primary" onClick={() => { this.toggleModal() } } >Do Something</Button>
+            </CardBody>
+          </Col>
         </Row>
+        <HotelModal setupToggleModal={this.setupModalToggle.bind(this)}></HotelModal>
       </Card>
     );
   }
@@ -78,3 +81,36 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(document.createElement('div')),
   )
 })
+
+class HotelModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.toggle = this.toggle.bind(this);
+    // allow parent (hotel div) to access child (hotel modal) function `toggle`
+    this.props.setupToggleModal(this.toggle);
+  }
+  toggle() {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }))
+  }
+  render() {
+    return (
+      <div>
+        <Modal isOpen={this.state.isOpen} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle} >Modal title</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle} >Book</Button>
+            <Button color="secondary" onClick={this.toggle} >Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
